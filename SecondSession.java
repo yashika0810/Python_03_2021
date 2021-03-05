7566c02c39939c5937b2aa305c0035617ad988b9
Yashika Khatri <yashikakhatri@Yashikas-MacBook-Pro.local>
2021-03-05 11:05:51 -0500

    assigned the value of b to 100

--- SecondSession.java
+++ SecondSession.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class SecondSession {
     public static void main(String[] args) {
 
+        
+
 
 
 
--- file.py
+++ file.py
@@ -1,6 +1,7 @@
 a=10
-b=50
+b=100
 result=a+b
 multiply=a*b
 print("Result is" result)
 print("Adding a new line")
+
